dd12b331d4b0126d2f8f9f4a34e825311a348795
Oleg <m160160@yandex.ru>
2021-03-06 00:12:26 +0700

    setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+module.exports = {
+    entry: {
+        index: "./resources/views/home/index.js",
+    },
+    module: {
+        rules: [
+            // babel
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        plugins: ["@babel/plugin-proposal-class-properties"],
+                        presets: ["@babel/preset-env"]
+                    },
+                },
+            },
+            // less
+            {
+                test: /\.less$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "less-loader"],
+            },
+            // css
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+    },
+    output: {
+        filename: "[name].[contenthash].bundle.js",
+        path: path.resolve(__dirname, "public", "build"),
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: 'resources/images',
+                    to: 'images'
+                },
+            ],
+        }),
+        new MiniCssExtractPlugin(),
+        new CleanWebpackPlugin(),
+    ],
+    resolve: {
+        modules: [
+            path.resolve(__dirname, "resources"),
+            "node_modules"
+        ],
+    },
+};
