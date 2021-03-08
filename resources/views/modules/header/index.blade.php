e46b797e4ca1ffc9912a93e0052e90186d487779
Oleg <m160160@yandex.ru>
2021-03-08 21:52:01 +0700

    add header

--- resources/views/modules/header/index.blade.php
+++ resources/views/modules/header/index.blade.php
@@ -1,3 +1,9 @@
 <div class="header">
-    Header
+    <div class="header__logo-container">
+        @include('icons.logo')
+    </div>
+    <div>Menu button</div>
+    <div>Search input</div>
+    <div>Profile</div>
+    <div>Cart</div>
 </div>
