812d752b1acbc7fdb5818acf94aa62864c7adb77
Oleg <m160160@yandex.ru>
2021-06-27 19:31:47 +0700

    add styles for offer item

--- resources/views/modules/offers/item/index.blade.php
+++ resources/views/modules/offers/item/index.blade.php
@@ -1,5 +1,12 @@
 <div class="offers-item">
-    {{$offer['title']}}
+    <div class="offers-item__image-block">
+        <div class="offers-item__image-container">
+            <img alt="{{$offer['title']}}" class="offers-item__image" src="{{$offer['image']}}">
+        </div>
+    </div>
+    <div class="offers-item__content-block">
+        {{$offer['title']}}
+    </div>
 </div>
 
 
