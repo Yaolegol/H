f6e9fecd6757f020a2dcc89c129aaccc464842eb
Oleg <m160160@yandex.ru>
2021-06-27 13:34:03 +0700

    rename ProductsController to SellerController

---
+++ resources/views/pages/sellers/index.blade.php
@@ -0,0 +1,16 @@
+@extends('modules.layout.index')
+
+@section('content')
+    @include('modules.header.catalog.index')
+    @include('modules.home.index')
+@endsection
+
+@section('scripts')
+    <script src="{{ asset('build/index.bundle.js') }}" defer></script>
+@endsection
+
+@section('styles')
+    <link href="{{ asset('build/index.css') }}" rel="stylesheet">
+@endsection
+
+
