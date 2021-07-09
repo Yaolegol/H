b89b367d3c0cfa35b0e8230e60211c3c8a1309d9
Oleg <m160160@yandex.ru>
2021-07-10 01:07:31 +0700

    remove useless functions

--- app/Http/Controllers/helpers/catalog/index.php
+++ app/Http/Controllers/helpers/catalog/index.php
@@ -8,67 +8,6 @@ use App\Models\Seller;
 function getCatalog()
 {
     return Catalog::all()->toArray();
-
-
-//    $catalogSecondLevel = CatalogSecondLevel::with('catalogFirstLevel')->get()->toArray();
-//    $catalog = [];
-//
-//    foreach ($catalogSecondLevel as $value) {
-//        $catalogFL = $value['catalog_first_level'];
-//        $catalogFLName = $catalogFL['link'];
-//        $catalogSecondLevelLink = '/' . 'catalog' . '/' . $catalogFL['link'];
-//
-//        if (!array_key_exists($catalogFLName, $catalog)) {
-//            $catalog[$catalogFLName] = [
-//                'content' => [
-//                    'categoriesList' => [
-//                        [
-//                            "id" => $value['id'],
-//                            "title" => $value['title'],
-//                            "link" => $catalogSecondLevelLink . '/' . $value['link'],
-//                            "image" => $value['image'],
-//                            "order" => $value['order'],
-//                        ],
-//                    ],
-//                    'title' => $value['catalog_first_level']['title'],
-//                ],
-//                'image' => $value['catalog_first_level']['image'],
-//                'link' => $catalogSecondLevelLink,
-//                'title' => $value['catalog_first_level']['title'],
-//            ];
-//        } else {
-//            array_push($catalog[$catalogFLName]['content']['categoriesList'],
-//                [
-//                    "id" => $value['id'],
-//                    "title" => $value['title'],
-//                    "link" => $catalogSecondLevelLink . '/' . $value['link'],
-//                    "image" => $value['image'],
-//                    "order" => $value['order'],
-//                ]
-//            );
-//        }
-//    }
-//    return $catalog;
-}
-
-function getCatalogLevel1()
-{
-    $catalog = getCatalog();
-
-    $catalogLevel1 = array_filter($catalog, function ($item) {
-        return $item["level"] === 1;
-    });
-
-    return array_map(function ($catalogItem) {
-        $catalogItemNew = getNewArray($catalogItem);
-        $catalogItemNew['link'] = 'catalog' . '/' . $catalogItemNew['link'];
-        return $catalogItemNew;
-    }, $catalogLevel1);
-}
-
-function getCatalogLevel2()
-{
-
 }
 
 function getCatalogFull()
@@ -144,26 +83,6 @@ function getCatalogLevel($level)
     return array_filter($catalog, function ($item) use ($level) {
         return $item["level"] === $level;
     });
-}
-
-function getCatalogSecondLevel($name)
-{
-    $catalog = getCatalog();
-    return [
-        'breadcrumbs' => [
-            [
-                'active' => false,
-                'link' => '/',
-                'title' => 'Каталог',
-            ],
-            [
-                'active' => true,
-                'link' => '/catalog/' . $name,
-                'title' => $catalog[$name]['title'],
-            ]
-        ],
-        'catalog' => $catalog[$name]['content']['categoriesList'],
-    ];
 }
 
 function getOffers($product)
