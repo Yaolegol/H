9ad3f075041e92e5059c880573b413a536b64776
Oleg <m160160@yandex.ru>
2021-07-10 21:19:07 +0700

    add country table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
+            CountrySeeder::class,
             CitySeeder::class,
             CatalogSeeder::class,
             SellerSeeder::class,
