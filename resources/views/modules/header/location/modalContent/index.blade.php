d373e7c4552837c4e7defdb3e9afb3b7f1cf7929
Oleg <m160160@yandex.ru>
2021-07-17 20:48:22 +0700

    add container for cities

--- resources/views/modules/header/location/modalContent/index.blade.php
+++ resources/views/modules/header/location/modalContent/index.blade.php
@@ -1,3 +1,6 @@
 <div class="header-location-modal-content">
     <h2 class="header-location-modal-content__title">Выберите город:</h2>
+    <div class="header-location-modal-content__content-container">
+
+    </div>
 </div>
