44cafff377189d558d6eac26d5566195f35550ec
Oleg <m160160@yandex.ru>
2021-07-17 22:17:13 +0700

    add with country for city

--- app/Models/City.php
+++ app/Models/City.php
@@ -11,9 +11,13 @@ class City extends Model
 
     protected $table = 'city';
 
+    public function country()
+    {
+        return $this->belongsTo(Country::class);
+    }
+
     public function region()
     {
         return $this->belongsTo(Region::class);
     }
-
 }
