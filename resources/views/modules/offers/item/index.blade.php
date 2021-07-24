00fb03fa19d0d0af0c371069b0719789d2ed890c
Oleg <m160160@yandex.ru>
2021-07-24 19:18:37 +0700

    style offer item card

--- resources/views/modules/offers/item/index.blade.php
+++ resources/views/modules/offers/item/index.blade.php
@@ -1,8 +1,6 @@
 <div class="offers-item">
     <div class="offers-item__image-block">
-        <div class="offers-item__image-container">
-            <img alt="{{$offer['title']}}" class="offers-item__image" src="{{$offer['image']}}">
-        </div>
+        <img alt="{{$offer['title']}}" class="offers-item__image" src="{{$offer['image']}}">
     </div>
     <div class="offers-item__content-block">
         <div class="offers-item__info-section">
