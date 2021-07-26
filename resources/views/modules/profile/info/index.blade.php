9605c38c5c26f83834964d057e27cdecc379f60a
Oleg <m160160@yandex.ru>
2021-07-26 17:27:06 +0700

    add profile info layout

--- resources/views/modules/profile/info/index.blade.php
+++ resources/views/modules/profile/info/index.blade.php
@@ -4,50 +4,60 @@
     </div>
     <div class="profile-info__content-block">
         <div class="profile-info__info-section">
-            <div>Имя</div>
-            <div>Test name</div>
-            <div>Телефон</div>
-            <div>8 111 111 11 11</div>
-            <div>Email</div>
-            <div>test@test.com</div>
-
-            <div>Наименование организации</div>
-            <div>Test organization name</div>
-            <div>ИНН организации</div>
-            <div>Test INN</div>
-            <div>Юридический адрес органицации</div>
-            <div>Test legal address</div>
-            <div>Фактический адрес органицации</div>
-            <div>Test fact address</div>
-            <div>Email</div>
-            <div>test@test.com</div>
-            <div>Телефон организации</div>
-            <div>8 222 222 22 22</div>
-            <div>Свидетельства</div>
-            <div>Test certificate</div>
-            <div>Фото</div>
-            <div>Test фото</div>
-
-            <div>Торговые точки</div>
-            <div>
-                <div>Адрес</div>
-                <div>test address</div>
-                <div>Контактное лицо</div>
-                <div>test person name</div>
-                <div>Телефон</div>
-                <div>8 333 333 33 33</div>
-                <div>Фото</div>
-                <div>Test фото</div>
+            <div class="profile-info__personal-info-container">
+                <h2>Личные данные</h2>
+                <div class="profile-info__info-title">Имя</div>
+                <div class="profile-info__info-description">Test name</div>
+                <div class="profile-info__info-title">Телефон</div>
+                <div class="profile-info__info-description">8 111 111 11 11</div>
+                <div class="profile-info__info-title">Email</div>
+                <div class="profile-info__info-description">test@test.com</div>
             </div>
-            <div>
-                <div>Адрес</div>
-                <div>test address</div>
-                <div>Контактное лицо</div>
-                <div>test person name</div>
-                <div>Телефон</div>
-                <div>8 333 333 33 33</div>
-                <div>Фото</div>
-                <div>Test фото</div>
+
+            <div class="profile-info__organization-info-container">
+                <h2>Данные об организации</h2>
+                <div class="profile-info__info-title">Наименование организации</div>
+                <div class="profile-info__info-description">Test organization name</div>
+                <div class="profile-info__info-title">ИНН организации</div>
+                <div class="profile-info__info-description">Test INN</div>
+                <div class="profile-info__info-title">Юридический адрес органицации</div>
+                <div class="profile-info__info-description">Test legal address</div>
+                <div class="profile-info__info-title">Фактический адрес органицации</div>
+                <div class="profile-info__info-description">Test fact address</div>
+                <div class="profile-info__info-title">Email организации</div>
+                <div class="profile-info__info-description">test@test.com</div>
+                <div class="profile-info__info-title">Телефон организации</div>
+                <div class="profile-info__info-description">8 222 222 22 22</div>
+                <div class="profile-info__info-title">Свидетельства</div>
+                <div class="profile-info__info-description">Test certificate</div>
+                <div class="profile-info__info-title">Фото</div>
+                <div class="profile-info__info-description">Test фото</div>
+            </div>
+
+            <div class="profile-info__sale-points-container">
+                <h2>Торговые точки</h2>
+                <div>
+                    <h3>Торговая точка 1</h3>
+                    <div class="profile-info__info-title">Адрес</div>
+                    <div class="profile-info__info-description">test address</div>
+                    <div class="profile-info__info-title">Контактное лицо</div>
+                    <div class="profile-info__info-description">test person name</div>
+                    <div class="profile-info__info-title">Телефон</div>
+                    <div class="profile-info__info-description">8 333 333 33 33</div>
+                    <div class="profile-info__info-title">Фото</div>
+                    <div class="profile-info__info-description">Test фото</div>
+                </div>
+                <div>
+                    <h3>Торговая точка 2</h3>
+                    <div class="profile-info__info-title">Адрес</div>
+                    <div class="profile-info__info-description">test address</div>
+                    <div class="profile-info__info-title">Контактное лицо</div>
+                    <div class="profile-info__info-description">test person name</div>
+                    <div class="profile-info__info-title">Телефон</div>
+                    <div class="profile-info__info-description">8 333 333 33 33</div>
+                    <div class="profile-info__info-title">Фото</div>
+                    <div class="profile-info__info-description">Test фото</div>
+                </div>
             </div>
         </div>
     </div>
