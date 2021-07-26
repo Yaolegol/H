a6b94963ef54a1ac456b5cb4e61e35162b1f3dbc
Oleg <m160160@yandex.ru>
2021-07-26 23:25:12 +0700

    add styles for profile-personal-info

--- resources/views/modules/profile/personal-info/index.blade.php
+++ resources/views/modules/profile/personal-info/index.blade.php
@@ -1,19 +1,17 @@
 <div class="profile-personal-info">
     <div class="profile-personal-info__image-block">
-        <img alt="test" class="profile-personal-info__image" src="test">
-    </div>
-    <div class="profile-personal-info__content-block">
-        <div class="profile-personal-info__info-section">
-            <div class="profile-personal-info__personal-info-container">
-                <h2>Личные данные</h2>
-                <div class="profile-personal-info__info-title">Имя</div>
-                <div class="profile-personal-info__info-description">Test name</div>
-                <div class="profile-personal-info__info-title">Телефон</div>
-                <div class="profile-personal-info__info-description">8 111 111 11 11</div>
-                <div class="profile-personal-info__info-title">Email</div>
-                <div class="profile-personal-info__info-description">test@test.com</div>
-            </div>
+        <div class="profile-personal-info__image-container">
+            <img alt="Photo" class="profile-personal-info__image" src="https://picsum.photos/200/300">
         </div>
+    </div>
+    <div class="profile-personal-info__content-container">
+        <h2>Личные данные</h2>
+        <div class="profile-personal-info__info-title">Имя</div>
+        <div class="profile-personal-info__info-description">Test name</div>
+        <div class="profile-personal-info__info-title">Телефон</div>
+        <div class="profile-personal-info__info-description">8 111 111 11 11</div>
+        <div class="profile-personal-info__info-title">Email</div>
+        <div class="profile-personal-info__info-description">test@test.com</div>
     </div>
 </div>
 
