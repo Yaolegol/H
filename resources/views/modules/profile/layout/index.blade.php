c53e2897904eb52cd517fa049e6a6322777ad0cf
Oleg <m160160@yandex.ru>
2021-07-26 23:08:35 +0700

    setup profile module structure

--- resources/views/modules/profile/layout/index.blade.php
+++ resources/views/modules/profile/layout/index.blade.php
@@ -1,36 +1,36 @@
-<div class="profile">
-    <div class="profile__title-container">
-        <h1 class="profile__title">Ваш профиль</h1>
+<div class="profile-layout">
+    <div class="profile-layout__title-container">
+        <h1 class="profile-layout__title">Ваш профиль</h1>
     </div>
-    <div class="profile__tabs-block">
-        <div class="profile__tabs-container">
-            <div class="profile__tab-item-container">
+    <div class="profile-layout__tabs-block">
+        <div class="profile-layout__tabs-container">
+            <div class="profile-layout__tab-item-container">
                 <a
-                    class="profile__tab-button profile__tab-button_active"
+                    class="profile-layout__tab-button profile-layout__tab-button_active"
                     href="/profile/personal-info"
                 >
                     Личные данные
                 </a>
             </div>
-            <div class="profile__tab-item-container">
+            <div class="profile-layout__tab-item-container">
                 <a
-                    class="profile__tab-button"
+                    class="profile-layout__tab-button"
                     href="/profile/organization-info"
                 >
                     Информация об организации
                 </a>
             </div>
-            <div class="profile__tab-item-container">
+            <div class="profile-layout__tab-item-container">
                 <a
-                    class="profile__tab-button"
+                    class="profile-layout__tab-button"
                     href="/profile/sale-points"
                 >
                     Информация о торговых точках
                 </a>
             </div>
-            <div class="profile__tab-item-container">
+            <div class="profile-layout__tab-item-container">
                 <a
-                    class="profile__tab-button"
+                    class="profile-layout__tab-button"
                     href="/profile/offers"
                 >
                     Мои торговые предложения
@@ -38,7 +38,7 @@
             </div>
         </div>
     </div>
-    <div class="profile__info-container">
+    <div class="profile-layout__info-container">
         @if($section === 'personal-info')
             @include('modules.profile.personal-info.index')
         @endif
--- resources/views/pages/profile/personal-info/index.js
+++ resources/views/pages/profile/personal-info/index.js
@@ -1,4 +1,5 @@
 import 'views/modules/header';
 import 'views/modules/layout';
 import 'views/modules/profile/layout';
+import 'views/modules/profile/personal-info';
 import './index.less';
