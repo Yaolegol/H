573a4a1b509c0e28e25f67ad03368c3fc04e50c0
Oleg <m160160@yandex.ru>
2021-07-26 22:27:28 +0700

    style profile tabs

--- resources/views/modules/profile/index.blade.php
+++ resources/views/modules/profile/index.blade.php
@@ -2,19 +2,41 @@
     <div class="profile__title-container">
         <h1 class="profile__title">Ваш профиль</h1>
     </div>
-    <div class="profile__tabs-container">
-        <a
-            class="profile__tab-button profile__tab-button_active"
-            href="/profile"
-        >
-            Личные данные
-        </a>
-        <a
-            class="profile__tab-button profile__tab-button_with-offset"
-            href="/profile"
-        >
-            Мои предложения
-        </a>
+    <div class="profile__tabs-block">
+        <div class="profile__tabs-container">
+            <div class="profile__tab-item-container">
+                <a
+                    class="profile__tab-button profile__tab-button_active"
+                    href="/profile"
+                >
+                    Личные данные
+                </a>
+            </div>
+            <div class="profile__tab-item-container">
+                <a
+                    class="profile__tab-button"
+                    href="/profile/organization"
+                >
+                    Информация об организации
+                </a>
+            </div>
+            <div class="profile__tab-item-container">
+                <a
+                    class="profile__tab-button"
+                    href="/profile/sale-points"
+                >
+                    Информация о торговых точках
+                </a>
+            </div>
+            <div class="profile__tab-item-container">
+                <a
+                    class="profile__tab-button"
+                    href="/profile/offers"
+                >
+                    Мои торговые предложения
+                </a>
+            </div>
+        </div>
     </div>
     <div class="profile__info-container">
         @include('modules.profile.info.index')
