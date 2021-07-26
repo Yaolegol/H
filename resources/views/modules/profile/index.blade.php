06cf48a5aa1242c0269f7bed68cd40eed1074af5
Oleg <m160160@yandex.ru>
2021-07-26 17:14:33 +0700

    add profile tabs

--- resources/views/modules/profile/index.blade.php
+++ resources/views/modules/profile/index.blade.php
@@ -2,6 +2,20 @@
     <div class="profile__title-container">
         <h1 class="profile__title">Ваш профиль</h1>
     </div>
+    <div class="profile__tabs-container">
+        <a
+            class="profile__tab-button profile__tab-button_active"
+            href="/profile"
+        >
+            Личные данные
+        </a>
+        <a
+            class="profile__tab-button profile__tab-button_with-offset"
+            href="/profile"
+        >
+            Мои предложения
+        </a>
+    </div>
     <div class="profile__info-container">
         @include('modules.profile.info.index')
     </div>
