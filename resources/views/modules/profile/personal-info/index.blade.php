5cd509580a77472ec4fc1c0a6c5f980ec62e35d8
Oleg <m160160@yandex.ru>
2021-07-26 23:11:54 +0700

    setup style names for profile module

--- resources/views/modules/profile/personal-info/index.blade.php
+++ resources/views/modules/profile/personal-info/index.blade.php
@@ -1,17 +1,17 @@
-<div class="personal-info">
-    <div class="personal-info__image-block">
-        <img alt="test" class="personal-info__image" src="test">
+<div class="profile-personal-info">
+    <div class="profile-personal-info__image-block">
+        <img alt="test" class="profile-personal-info__image" src="test">
     </div>
-    <div class="personal-info__content-block">
-        <div class="personal-info__info-section">
-            <div class="personal-info__personal-info-container">
+    <div class="profile-personal-info__content-block">
+        <div class="profile-personal-info__info-section">
+            <div class="profile-personal-info__personal-info-container">
                 <h2>Личные данные</h2>
-                <div class="personal-info__info-title">Имя</div>
-                <div class="personal-info__info-description">Test name</div>
-                <div class="personal-info__info-title">Телефон</div>
-                <div class="personal-info__info-description">8 111 111 11 11</div>
-                <div class="personal-info__info-title">Email</div>
-                <div class="personal-info__info-description">test@test.com</div>
+                <div class="profile-personal-info__info-title">Имя</div>
+                <div class="profile-personal-info__info-description">Test name</div>
+                <div class="profile-personal-info__info-title">Телефон</div>
+                <div class="profile-personal-info__info-description">8 111 111 11 11</div>
+                <div class="profile-personal-info__info-title">Email</div>
+                <div class="profile-personal-info__info-description">test@test.com</div>
             </div>
         </div>
     </div>
