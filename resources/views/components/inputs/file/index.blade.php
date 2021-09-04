a11e623c396545d95764030758d14fad9d9fb6f8
Oleg <m160160@yandex.ru>
2021-09-04 17:02:08 +0700

    add preview for file input

--- resources/views/components/inputs/file/index.blade.php
+++ resources/views/components/inputs/file/index.blade.php
@@ -1,9 +1,12 @@
 <div class="inputs-file j-inputs-file">
-    <label class="inputs-file__label" for="file-input-{{$name}}">{{$title}}</label>
-    <input
-            class="inputs-file__input"
+    <div class="inputs-file__image-section j-inputs-file__image-container"></div>
+    <div class="inputs-file__input-section">
+        <label class="inputs-file__label" for="file-input-{{$name}}">{{$title}}</label>
+        <input
+            class="inputs-file__input j-inputs-file__input"
             id="file-input-{{$name}}"
             name="{{$name}}"
             type="file"
-    >
+        >
+    </div>
 </div>
--- resources/views/components/inputs/file/index.js
+++ resources/views/components/inputs/file/index.js
@@ -1,8 +1,38 @@
+import {addEventListener} from 'helpers/events';
 import './index.less';
 
 class InputsFile {
     constructor(element) {
         this.module = element;
+        this.imageContainer = this.module.querySelector('.j-inputs-file__image-container');
+        this.input = this.module.querySelector('.j-inputs-file__input');
+
+        this.bind();
+    }
+
+    bind = () => {
+        addEventListener(this.input, 'change', this.handleInputChange);
+    }
+
+    addImageToHTML = (src) => {
+        const image = `
+            <img
+                alt=""
+                class="inputs-file__image"
+                src="${src}"
+            >
+        `;
+
+        this.imageContainer.insertAdjacentHTML('afterbegin', image);
+    }
+
+    handleInputChange = (e) => {
+        const file = e.target.files[0];
+
+        if(file) {
+            const src = URL.createObjectURL(file);
+            this.addImageToHTML(src);
+        }
     }
 }
 
