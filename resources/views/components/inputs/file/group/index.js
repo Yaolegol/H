e61a4340e4df819fe30fcea92a46a508ffe1f3fa
Oleg <m160160@yandex.ru>
2021-09-05 16:26:39 +0700

    remove useless code

--- resources/views/components/inputs/file/group/index.js
+++ resources/views/components/inputs/file/group/index.js
@@ -1,15 +1,2 @@
-import {addEventListener} from 'helpers/events';
 import 'views/components/inputs/file/item';
 import './index.less';
-
-class InputsFileGroup {
-    constructor(element) {
-        this.module = element;
-    }
-}
-
-const list = [...document.querySelectorAll('.j-inputs-file-group')];
-
-list.forEach((element) => {
-    new InputsFileGroup(element);
-});
