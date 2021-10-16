7ac4058efe2209b381c698fa1558394886dca0ba
Oleg <m160160@yandex.ru>
2021-10-16 21:19:03 +0700

    set up leaflet

--- resources/views/modules/profile/sale-offers/create/index.js
+++ resources/views/modules/profile/sale-offers/create/index.js
@@ -1,8 +1,17 @@
 import 'views/components/inputs/radio/content-group';
 import 'views/components/inputs/radio/group';
-import 'leaflet/dist/leaflet';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'views/components/inputs/radio/group';
 import './index.less';
 
 const map = L.map('leaflet-map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1
+}).addTo(map);
