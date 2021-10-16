db72eecf38492177e57434486ef58d1c110d4be9
Oleg <m160160@yandex.ru>
2021-10-16 22:18:02 +0700

    add map position

--- resources/views/modules/profile/sale-offers/create/index.js
+++ resources/views/modules/profile/sale-offers/create/index.js
@@ -5,7 +5,7 @@ import 'leaflet/dist/leaflet.css';
 import 'views/components/inputs/radio/group';
 import './index.less';
 
-const map = L.map('leaflet-map').setView([51.505, -0.09], 13);
+const map = L.map('leaflet-map').setView([56.486932, 84.944716], 36);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
