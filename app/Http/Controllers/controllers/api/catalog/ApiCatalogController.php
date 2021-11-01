94668d2e4ea8006665379e1e4f920d9140b592e2
Oleg <m160160@yandex.ru>
2021-11-02 03:09:11 +0700

    add ApiCatalogController

---
+++ app/Http/Controllers/controllers/api/catalog/ApiCatalogController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers\controllers\api\catalog;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Controllers\Controller;
+
+require_once('app/Http/Controllers/helpers/catalog/index.php');
+
+class ApiCatalogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        $catalogFull = getCatalogFull();
+
+        return json_encode($catalogFull, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  string  $catalogLevelOneLink
+     * @return Response
+     */
+    public function show($catalogLevelOneLink)
+    {
+        $catalogFull = getCatalogFull();
+        $catalogLevelOneItemSubcategoriesList = getCatalogLevelOneItemSubcategoriesList($catalogFull, $catalogLevelOneLink);
+        $breadcrumbs = getCatalogLevelTwoBreadcrumbs($catalogFull, $catalogLevelOneLink);
+        $locationList = getLocationListFormatted();
+
+        return view('pages.catalog.secondLevel.index', [
+            'breadcrumbs' => $breadcrumbs,
+            'catalogHeader' => $catalogFull,
+            'catalogPage' => $catalogLevelOneItemSubcategoriesList,
+            'locationList' => $locationList,
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  Request  $request
+     * @param  int  $id
+     * @return Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
