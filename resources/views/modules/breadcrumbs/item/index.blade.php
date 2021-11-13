e3fe3c85d701b22356e0f835356a26eaa19a34e2
Oleg <m160160@yandex.ru>
2021-11-13 15:33:32 +0700

    fix breadcrumbs

--- resources/views/modules/breadcrumbs/item/index.blade.php
+++ resources/views/modules/breadcrumbs/item/index.blade.php
@@ -3,7 +3,7 @@
         <span>{{$breadcrumbsItem['title']}}</span>
     @else
         <a class="breadcrumbs-item__link" href="{{$breadcrumbsItem['link']}}">{{$breadcrumbsItem['title']}}</a>
-        <span class="breadcrumbs-item__arrow">-></span>
+        <div class="breadcrumbs-item__arrow">></div>
     @endif
 </div>
 
