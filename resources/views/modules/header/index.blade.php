4125c04dfb624137ea41e32ee07f69ad24a9c420
Oleg <m160160@yandex.ru>
2021-11-17 23:42:45 +0700

    add map block for header

--- resources/views/modules/header/index.blade.php
+++ resources/views/modules/header/index.blade.php
@@ -1,37 +1,44 @@
 @extends('components.modals.common.index')
 
 <div class="header">
-    <div class="header__logo-container">
-        @include('icons.logo')
-    </div>
-    <div>
-        @include('components.buttons.burger.index')
-    </div>
-    <div>
-        @include('components.inputs.search.index')
-    </div>
-    <div class="header__location-container">
-        @include('components.buttons.location.index')
-    </div>
-    <div class="header__favorites-container">
-        @include('icons.favorite')
-    </div>
-    <div class="header__profile-container">
-        <a class="header__profile-link" href="/profile">
-            @include('icons.profile')
-        </a>
-    </div>
-    @auth
-        <div class="header__login-container">
-            <a class="header__login-link" href="/logout">Выйти</a>
+    <div class="header__map-block">
+        <div class="header__map-container">
+            <a class="header__map-link" href="/">Карта</a>
         </div>
-    @endauth
+    </div>
+    <div class="header__main">
+        <div class="header__logo-container">
+            @include('icons.logo')
+        </div>
+        <div>
+            @include('components.buttons.burger.index')
+        </div>
+        <div>
+            @include('components.inputs.search.index')
+        </div>
+        <div class="header__location-container">
+            @include('components.buttons.location.index')
+        </div>
+        <div class="header__favorites-container">
+            @include('icons.favorite')
+        </div>
+        <div class="header__profile-container">
+            <a class="header__profile-link" href="/profile">
+                @include('icons.profile')
+            </a>
+        </div>
+        @auth
+            <div class="header__login-container">
+                <a class="header__login-link" href="/logout">Выйти</a>
+            </div>
+        @endauth
 
-    @guest
-        <div class="header__login-container">
-            <a class="header__login-link" href="/login">Войти</a>
-        </div>
-    @endguest
+        @guest
+            <div class="header__login-container">
+                <a class="header__login-link" href="/login">Войти</a>
+            </div>
+        @endguest
+    </div>
 </div>
 
 @section('modals-common-content')
