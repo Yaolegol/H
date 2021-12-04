46c4c44df801bb8ca70e62dd181ce1403d199585
Oleg <m160160@yandex.ru>
2021-12-04 22:03:40 +0700

    add styles

--- resources/views/modules/map/web/index/index.blade.php
+++ resources/views/modules/map/web/index/index.blade.php
@@ -1,8 +1,8 @@
 <div class="map-web-index">
-    <div class="offers__location-container">
-        @include('modules.location.index')
-    </div>
     <div class="map-web-index__content-area">
+        <div class="map-web-index__location-container">
+            @include('modules.location.index')
+        </div>
         <div class="map-web-index__info-item-container">
             <div>Где купить?</div>
             <div>
