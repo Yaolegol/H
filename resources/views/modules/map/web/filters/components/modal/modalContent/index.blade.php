f58eb854d48300f76d6ef70dcad03948d15f4656
Oleg <m160160@yandex.ru>
2021-12-07 20:16:35 +0700

    add styles

--- resources/views/modules/map/web/filters/components/modal/modalContent/index.blade.php
+++ resources/views/modules/map/web/filters/components/modal/modalContent/index.blade.php
@@ -1,28 +1,31 @@
 <div class="map-web-filters-components-modal-modal-content j-location-modal-content">
-    <h2 class="map-web-filters-components-modal-modal-content__title">Выберите категорию:</h2>
-    <div class="map-web-filters-components-modal-modal-content__content-container">
-        @component('components.catalog.index')
-            @component('components.catalog.navigation-item-container.index')
-                @foreach($catalogHeader as $catalogItem)
-                    @component('components.catalog.navigation-item.index', [ 'itemId' => $loop->index ])
-                        {{ $catalogItem['title'] }}
-                    @endcomponent
-                @endforeach
+    <h2 class="map-web-filters-components-modal-modal-content__title">Фильтры</h2>
+    <div class="map-web-filters-components-modal-modal-content__filter-item-container">
+        <div class="map-web-filters-components-modal-modal-content__filter-item-title">Категория:</div>
+        <div class="map-web-filters-components-modal-modal-content__content-container">
+            @component('components.catalog.index')
+                @component('components.catalog.navigation-item-container.index')
+                    @foreach($catalogHeader as $catalogItem)
+                        @component('components.catalog.navigation-item.index', [ 'itemId' => $loop->index ])
+                            {{ $catalogItem['title'] }}
+                        @endcomponent
+                    @endforeach
+                @endcomponent
+                @component('components.catalog.content-item-container.index')
+                    @foreach($catalogHeader as $catalogItem)
+                        @component('components.catalog.content-item.index', [ 'itemId' => $loop->index ])
+                            <div>{{ $catalogItem['title'] }}</div>
+                            <div class="components-catalog__categories-container">
+                                @foreach( $catalogItem['catalog_level_two'] as $category )
+                                    @component('components.catalog.category-item.index')
+                                        {{ $category['title'] }}
+                                    @endcomponent
+                                @endforeach
+                            </div>
+                        @endcomponent
+                    @endforeach
+                @endcomponent
             @endcomponent
-            @component('components.catalog.content-item-container.index')
-                @foreach($catalogHeader as $catalogItem)
-                    @component('components.catalog.content-item.index', [ 'itemId' => $loop->index ])
-                        <div>{{ $catalogItem['title'] }}</div>
-                        <div class="components-catalog__categories-container">
-                            @foreach( $catalogItem['catalog_level_two'] as $category )
-                                @component('components.catalog.category-item.index')
-                                    {{ $category['title'] }}
-                                @endcomponent
-                            @endforeach
-                        </div>
-                    @endcomponent
-                @endforeach
-            @endcomponent
-        @endcomponent
+        </div>
     </div>
 </div>
