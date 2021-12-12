a900e3433fe4ffcdc0b5061d58cbb12ce8694c0a
Oleg <m160160@yandex.ru>
2021-12-12 22:35:16 +0700

    init

--- resources/views/modules/layout/index.blade.php
+++ resources/views/modules/layout/index.blade.php
@@ -11,8 +11,51 @@
 
         <!-- Styles -->
         @yield('layout-styles')
+
+        <script data-id="6">
+            console.log('HEADER LOG')
+            console.log('document.readyState')
+            console.log(document.readyState)
+
+            document.addEventListener('readystatechange', (event) => {
+                console.log('--- readystatechange')
+                console.log(document.readyState)
+            });
+
+            window.addEventListener('load', (event) => {
+                console.log('--- load');
+            });
+
+            document.addEventListener("DOMContentLoaded", function(event) {
+                console.log("--- DOMContentLoaded");
+            });
+        </script>
+        <script data-id="3">
+            console.log('START COUNTING');
+            let count = 0;
+            while(count < 1000000000) {
+                count++;
+            }
+
+            console.log(count);
+        </script>
+
+        <script data-id="11" src="/build/test_header_medium.js"></script>
+        <script data-id="4" src="/build/test_header.js"></script>
+        <script data-id="10" src="/build/test_header_small.js"></script>
     </head>
     <body>
+        <script data-id="7">
+            console.log('BODY START')
+            console.log('document.readyState')
+            console.log(document.readyState)
+
+            let scriptTag = document.createElement('script');
+            scriptTag.src = '/build/test_header_0.js';
+            scriptTag.async = false;
+            scriptTag.defer = true;
+            document.head.append(scriptTag);
+        </script>
         @auth
             <div class="j-user__auth"></div>
         @endauth
@@ -25,7 +68,44 @@
                 </div>
             </div>
 
-            @yield('layout-scripts')
+
         </div>
+
+
+        <div class="j-test-1">test</div>
+        @yield('layout-scripts')
+        <script data-id="1">
+            console.log('BODY LOG')
+            console.log('document.readyState')
+            console.log(document.readyState)
+
+            const test1 = document.querySelector('.j-test-1');
+            console.log('test1');
+            console.log(test1);
+
+            const test_script_1 = () => {
+                console.log('test_script_1');
+            };
+
+            const test2 = document.querySelector('.j-test-2');
+            console.log('test2');
+            console.log(test2);
+            test2.classList.add('test');
+        </script>
+
+        <script data-id="2">
+            test_script_1();
+        </script>
+        <script data-id="5" src="/build/test_body_bottom.js"></script>
+
+        <div class="j-test-2">test 2</div>
+        <script data-id="8">
+            const newTest2 = document.querySelector('.j-test-2');
+            console.log('!!! test2');
+            console.log(newTest2);
+        </script>
+        <div>test</div>
+        <div>test</div>
+
     </body>
 </html>
