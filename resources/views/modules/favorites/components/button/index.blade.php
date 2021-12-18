af0fbc4a4d3bf2265850f3503ad51ada794b08d1
Oleg <m160160@yandex.ru>
2021-12-18 19:53:50 +0700

    add favorites hint block

--- resources/views/modules/favorites/components/button/index.blade.php
+++ resources/views/modules/favorites/components/button/index.blade.php
@@ -5,4 +5,18 @@
     <button class="favorites-components-button__button j-favorites-components-button__button">
         @include('icons.favorite')
     </button>
+    @guest
+        <div class="favorites-components-button__hint-block">
+            <div class="favorites-components-button__hint-title">Чтобы добавить товар в избранное нужно</div>
+            <div class="favorites-components-button__hint-text-container">
+                <a class="favorites-components-button__hint-link" href="/login">Войти</a>
+            </div>
+            <div class="favorites-components-button__hint-text-container">
+                <div class="favorites-components-button__hint-text">или</div>
+            </div>
+            <div class="favorites-components-button__hint-text-container">
+                <a class="favorites-components-button__hint-link" href="/register">Зарегистрироваться</a>
+            </div>
+        </div>
+    @endguest
 </div>
