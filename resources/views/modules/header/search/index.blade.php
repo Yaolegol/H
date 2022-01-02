84b35942cfdea5e2eaf7a77c0f6d828ee55f8e39
Oleg <m160160@yandex.ru>
2022-01-03 02:10:42 +0700

    style search results block

--- resources/views/modules/header/search/index.blade.php
+++ resources/views/modules/header/search/index.blade.php
@@ -1,12 +1,26 @@
 <div class="inputs-search j-header-search">
-    <input
+    <div class="inputs-search__input-container">
+        <input
             class="inputs-search__input j-header-search__input"
             placeholder="Search"
             type="text"
-    >
-    <button class="inputs-search__button j-header-search__send-button" type="button">
+        >
+        <button class="inputs-search__button j-header-search__send-button" type="button">
         <span class="inputs-search__button-icon">
             @include('icons.search')
         </span>
-    </button>
+        </button>
+    </div>
+    <div class="inputs-search__search-results-block">
+        <div class="inputs-search__search-results-container">
+            <div class="inputs-search__search-results-categories-container">
+                <div>Категории</div>
+                <div>test</div>
+            </div>
+            <div class="inputs-search__search-results-sellers-container">
+                <div>Продавцы</div>
+                <div>test</div>
+            </div>
+        </div>
+    </div>
 </div>
