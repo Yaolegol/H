8ba90893879747b21832d6fdf0d74bce4cae364f
Oleg <m160160@yandex.ru>
2022-01-04 02:08:23 +0700

    add styles

--- resources/views/modules/header/search/index.blade.php
+++ resources/views/modules/header/search/index.blade.php
@@ -11,11 +11,11 @@
         <div class="inputs-search__search-results-block">
             <div class="inputs-search__search-results-container hidden j-header-search__search-results-categories-container">
                 <div>Категории</div>
-                <div class="j-header-search__search-results-categories-result-container"></div>
+                <div class="inputs-search__result-container j-header-search__search-results-categories-result-container"></div>
             </div>
             <div class="inputs-search__search-results-container hidden j-header-search__search-results-sellers-container">
                 <div>Продавцы</div>
-                <div class="j-header-search__search-results-sellers-result-container"></div>
+                <div class="inputs-search__result-container j-header-search__search-results-sellers-result-container"></div>
             </div>
             <div class="inputs-search__search-results-container hidden j-header-search__search-results-non-container">
                 <div>Найдено 0 результатов</div>
