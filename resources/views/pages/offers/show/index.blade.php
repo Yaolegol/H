157cc395bdc379d491570f5e448751bf4cc947f5
Oleg <m160160@yandex.ru>
2022-01-05 21:33:28 +0700

    offers fix show page layout

--- resources/views/pages/offers/show/index.blade.php
+++ resources/views/pages/offers/show/index.blade.php
@@ -2,7 +2,9 @@
 
 @section('layout-content')
     @include('modules.header.catalog.index')
-    @include('modules.offers.offer.index')
+    @component('components.page.common.container.index')
+        @include('modules.offers.offer.index')
+    @endcomponent
 @endsection
 
 @section('layout-scripts')
