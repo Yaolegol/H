0eab9b18dcd3f4788622469d7bdf0468e87f877e
Oleg <m160160@yandex.ru>
2022-01-07 04:09:07 +0700

    fix favorites css class names

--- resources/views/modules/pages/favorites/shared/components/button/index.blade.php
+++ resources/views/modules/pages/favorites/shared/components/button/index.blade.php
@@ -1,31 +1,31 @@
 <div
-    class="favorites-components-button j-favorites-components-button"
+    class="modules-pages-favorites-shared-components-button j-favorites-components-button"
     data-id="{{$id}}"
 >
-    <button class="favorites-components-button__button j-favorites-components-button__button">
+    <button class="modules-pages-favorites-shared-components-button__button j-favorites-components-button__button">
         @include('icons.favorite')
     </button>
     @guest
         <div
             class="
-                favorites-components-button__hint-block
+                modules-pages-favorites-shared-components-button__hint-block
                 @isset($hintPosition)
                     {{$hintPosition === 'left' ?
-                    'favorites-components-button__hint-block_left' :
+                    'modules-pages-favorites-shared-components-button__hint-block_left' :
                     ''
                     }}
                 @endisset
             "
         >
-            <div class="favorites-components-button__hint-title">Чтобы добавить товар в избранное нужно</div>
-            <div class="favorites-components-button__hint-text-container">
-                <a class="favorites-components-button__hint-link" href="/login">Войти</a>
+            <div class="modules-pages-favorites-shared-components-button__hint-title">Чтобы добавить товар в избранное нужно</div>
+            <div class="modules-pages-favorites-shared-components-button__hint-text-container">
+                <a class="modules-pages-favorites-shared-components-button__hint-link" href="/login">Войти</a>
             </div>
-            <div class="favorites-components-button__hint-text-container">
-                <div class="favorites-components-button__hint-text">или</div>
+            <div class="modules-pages-favorites-shared-components-button__hint-text-container">
+                <div class="modules-pages-favorites-shared-components-button__hint-text">или</div>
             </div>
-            <div class="favorites-components-button__hint-text-container">
-                <a class="favorites-components-button__hint-link" href="/register">Зарегистрироваться</a>
+            <div class="modules-pages-favorites-shared-components-button__hint-text-container">
+                <a class="modules-pages-favorites-shared-components-button__hint-link" href="/register">Зарегистрироваться</a>
             </div>
         </div>
     @endguest
