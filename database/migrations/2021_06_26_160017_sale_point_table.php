71b8aa30878e7a9e11be0f8e0d0ca625e3bf3b2a
Oleg <m160160@yandex.ru>
2022-04-03 00:44:17 +0700

    fix SalePointTable

--- database/migrations/2021_06_26_160017_sale_point_table.php
+++ database/migrations/2021_06_26_160017_sale_point_table.php
@@ -16,6 +16,7 @@ class SalePointTable extends Migration
         Schema::create('sale_point', function (Blueprint $table) {
             $table->id();
             $table->string('title')->nullable();
+            $table->string('description')->nullable();
             $table->string('address')->nullable();
             $table->string('working_hours')->nullable();
             $table->string('contact_person')->nullable();
