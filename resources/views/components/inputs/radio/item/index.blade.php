e8ff941bd8e135b1dae1dac42163259fedd516ae
Oleg <m160160@yandex.ru>
2022-04-02 22:43:57 +0700

    add styles for radio

--- resources/views/components/inputs/radio/item/index.blade.php
+++ resources/views/components/inputs/radio/item/index.blade.php
@@ -1,16 +1,21 @@
 <div class="components-inputs-radio-item">
     <label
-        class="components-inputs-radio-item__category-input-label"
+        class="components-inputs-radio-item__input-label"
     >
         <input
             @if($isChecked ?? false)
                 checked
             @endif
-            class="components-inputs-radio-item__category-input"
+            class="components-inputs-radio-item__input"
             name="{{$name}}"
             type="radio"
             value="{{$value}}"
         >
-        <span>{{$title}}</span>
+        <span class="components-inputs-radio-item__marker-block">
+            <span class="components-inputs-radio-item__marker-container">
+                @include('icons.checkmark')
+            </span>
+        </span>
+        <span class="components-inputs-radio-item__title">{{$title}}</span>
     </label>
 </div>
---
+++ resources/views/icons/checkmark.blade.php
@@ -0,0 +1,3 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" viewBox="0 0 48 48">
+    <polygon fill="currentColor" points="40.6,12.1 17,35.7 7.4,26.1 4.6,29 17,41.3 43.4,14.9" />
+</svg>
