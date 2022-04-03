f6aa83daad15e5c693ad9c32b678cbc2d42fd41d
Oleg <m160160@yandex.ru>
2022-04-03 19:12:00 +0700

    include maps + fix styles for view item map

--- resources/views/components/map/2gis/components/add-marker/index.js
+++ resources/views/components/map/2gis/components/add-marker/index.js
@@ -107,6 +107,5 @@ class Map2gisComponentsAddMarker {
 const list = [...document.querySelectorAll('.j-map-2gis-components-add-marker')];
 
 list.forEach((element) => {
-    return;
     new Map2gisComponentsAddMarker(element);
 })
--- resources/views/components/map/2gis/components/viewItem/index.js
+++ resources/views/components/map/2gis/components/viewItem/index.js
@@ -78,6 +78,5 @@ class Map2gisComponentsViewItem {
 const list = [...document.querySelectorAll('.j-map-2gis-components-view-item')];
 
 list.forEach((element) => {
-    return;
     new Map2gisComponentsViewItem(element);
 })
