7bbbfda10e110aba9df5ce67fc1fddf2662cc356
Oleg <m160160@yandex.ru>
2022-05-07 22:02:54 +0700

    fix search placeholder

--- resources/views/modules/common/header/search/index.blade.php
+++ resources/views/modules/common/header/search/index.blade.php
@@ -9,7 +9,7 @@
         <label class="modules-common-header-search__search-container j-header-search__search-container">
             <input
                 class="modules-common-header-search__input j-header-search__input"
-                placeholder="Найти категорию или продавца"
+                placeholder="Найти"
                 type="text"
             />
             <button
