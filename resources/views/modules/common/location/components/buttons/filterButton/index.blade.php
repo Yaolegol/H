cd2c9e24047cf41d8dcff1143a338bfd1213519f
Oleg <m160160@yandex.ru>
2022-08-01 16:57:03 +0700

    refactor resources/views/modules/common/location/components/buttons/filterButton/index.blade.php

--- resources/views/modules/common/location/components/buttons/filterButton/index.blade.php
+++ resources/views/modules/common/location/components/buttons/filterButton/index.blade.php
@@ -1,6 +1,17 @@
+@php
+    $cityData = $locationSearchData['city'];
+    $regionData = $locationSearchData['region'];
+
+    $title = 'Все регионы';
+
+    if($cityData) {
+        $title = $cityData['title'];
+    } elseif($regionData) {
+        $title = $regionData['title'];
+    }
+@endphp
 <div class="modules-common-location-components-choose-filter-button j-modules-common-location-components-choose-filter-button">
-    @if(isset($locationSearchData['city']))
-        @include('components.buttons.filter.index', [
+    @include('components.buttons.filter.index', [
                 'className' => 'j-components-buttons-modal-open',
                 'dataset' => [
                     [
@@ -9,31 +20,6 @@
                     ],
                 ],
                 'defaultTitle' => 'Все регионы',
-                'title' => $locationSearchData['city']['title'],
+                'title' => $title,
             ])
-    @elseif(isset($locationSearchData['region']))
-        @include('components.buttons.filter.index', [
-               'className' => 'j-components-buttons-modal-open',
-               'dataset' => [
-                    [
-                        'key' => 'data-template-id',
-                        'value' => 'location',
-                    ],
-                ],
-               'defaultTitle' => 'Все регионы',
-               'title' => $locationSearchData['region']['title'],
-           ])
-    @else
-        @include('components.buttons.filter.index', [
-                'className' => 'j-components-buttons-modal-open',
-                'dataset' => [
-                    [
-                        'key' => 'data-template-id',
-                        'value' => 'location',
-                    ],
-                ],
-                'defaultTitle' => 'Все регионы',
-                'title' => 'Все регионы',
-            ])
-    @endif
 </div>
