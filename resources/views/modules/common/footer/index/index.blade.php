089c163578e42141d256622f77b2e55031b529f2
Oleg <m160160@yandex.ru>
2022-08-02 23:37:02 +0700

    fix footer + adaptive styles for product page

--- resources/views/modules/common/footer/index/index.blade.php
+++ resources/views/modules/common/footer/index/index.blade.php
@@ -1,11 +1,11 @@
 <div class="modules-common-footer-index">
     <div class="modules-common-footer-index__info-container">
-        <div>© 2022 г. Все права защищены</div>
-        <div class="modules-common-footer-index__rules-container">
+        <div>
             <a
                 class="modules-common-footer-index__link"
                 href="/"
             >Правила пользования сайтом и политика конфиденциальности</a>
         </div>
+        <div class="modules-common-footer-index__copyright-container">© 2022 г. Все права защищены</div>
     </div>
 </div>
