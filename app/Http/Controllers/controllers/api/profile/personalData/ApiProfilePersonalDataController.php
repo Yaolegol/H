75851ca58618b8f21e681ce2107c12a45c7d25e9
Oleg <m160160@yandex.ru>
2022-08-07 20:30:38 +0700

    fix change registration email and password api

--- app/Http/Controllers/controllers/api/profile/personalData/ApiProfilePersonalDataController.php
+++ app/Http/Controllers/controllers/api/profile/personalData/ApiProfilePersonalDataController.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\Controller;
 require_once('app/Http/Controllers/helpers/api/profile/personalData/index.php');
 require_once('app/Http/Controllers/helpers/common/assets/index.php');
 require_once('app/Http/Controllers/helpers/common/errors/index.php');
+require_once('app/Http/Controllers/helpers/common/user/index.php');
 require_once('app/Http/Controllers/helpers/web/profile/personalData/index.php');
 
 class ApiProfilePersonalDataController extends Controller
@@ -149,61 +150,41 @@ class ApiProfilePersonalDataController extends Controller
     public function updatePersonalEmail(Request $request)
     {
         $currentPassword = $request->input('password');
-
-        $validator = Validator::make(
-            $request->all(),
-            [
-                'registration_email' => ['required', 'email', 'max:25', 'unique:users'],
-                'password' => ['required', 'min:6'],
-            ],
-            [
-                'email' => 'Поле должно содержать email',
-                'max' => 'Поле должно содержать максимум :max символов',
-                'min' => 'Поле должно содержать минимум :min символов',
-                'required' => 'Поле обязательно для заполнения',
-                'unique' => 'Пользователь с таким Email уже зарегистрирован',
-            ]
-        );
+        $validator = getEmailValidator($request);
 
         if($validator->fails()) {
             $data = [
                 'data' => '',
-                'errors' => $validator->errors(),
+                'errors' => getValidatorErrorsList($validator),
             ];
 
-            return json_encode($data, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT);
+            return json_encode($data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+
         }
 
-        if(Hash::check($currentPassword, Auth::user()->password)) {
+        $isAuth = checkAuthUserPassword($currentPassword);
+
+        if($isAuth) {
             $isSaved = DB_tryChangeUserEmail($request);
 
             if($isSaved) {
                 $data = [
                     'data' => '',
-                    'errors' => '',
+                    'errors' => [],
                 ];
 
-                return json_encode($data, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT);
-            } else {
-                $data = [
-                    'data' => '',
-                    'errors' => [
-                        'common' => 'Что-то пошло не так. Попробуйте снова',
-                    ],
-                ];
+                return json_encode($data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
 
-                return json_encode($data, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT);
             }
-        } else {
-            $data = [
-                'data' => '',
-                'errors' => [
-                    'common' => 'Неверный пароль',
-                ],
-            ];
-
-            return json_encode($data, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT);
         }
+
+        $data = [
+            'data' => '',
+            'errors' => ['Что-то пошло не так. Попробуйте снова'],
+        ];
+
+        return json_encode($data, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT);
+
     }
 
     /**
@@ -214,59 +195,37 @@ class ApiProfilePersonalDataController extends Controller
      */
     public function updatePersonalPassword(Request $request) {
         $currentPassword = $request->input('current_password');
-
-        $validator = Validator::make(
-            $request->all(),
-            [
-                'current_password' => ['required', 'min:6'],
-                'password' => ['required', 'min:6'],
-                'password_confirmation' => ['required', 'same:password'],
-            ],
-            [
-                'min' => 'Поле должно содержать минимум :min символов',
-                'required' => 'Поле обязательно для заполнения',
-                'same' => 'Поля Password и Confirm Password не совпадают',
-            ]
-        );
+        $validator = getPasswordValidator($request);
 
         if($validator->fails()) {
             $data = [
                 'data' => '',
-                'errors' => $validator->errors(),
+                'errors' => getValidatorErrorsList($validator),
             ];
 
             return json_encode($data, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT);
         }
 
-        if(Hash::check($currentPassword, Auth::user()->password)) {
-            $isSaved = tryChangeUserPasswordInDB($request);
+        $isAuth = checkAuthUserPassword($currentPassword);
+
+        if($isAuth) {
+            $isSaved = DB_tryChangeUserPassword($request);
 
             if($isSaved) {
                 $data = [
                     'data' => '',
-                    'errors' => '',
-                ];
-
-                return json_encode($data, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT);
-            } else {
-                $data = [
-                    'data' => '',
-                    'errors' => [
-                        'common' => 'Что-то пошло не так. Попробуйте снова',
-                    ],
+                    'errors' => [],
                 ];
 
                 return json_encode($data, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT);
             }
-        } else {
-            $data = [
-                'data' => '',
-                'errors' => [
-                    'common' => 'Неверный пароль',
-                ],
-            ];
-
-            return json_encode($data, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT);
         }
+
+        $data = [
+            'data' => '',
+            'errors' => ['Что-то пошло не так. Попробуйте снова'],
+        ];
+
+        return json_encode($data, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT);
     }
 }
