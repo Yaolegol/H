6a570c2452bc345c4419d2555fd85524d24ae573
Oleg <m160160@yandex.ru>
2022-08-21 00:12:19 +0700

    add yandex map and mobile single poin page

--- resources/views/pages/map/mobileApp/singlePoint/index.blade.php
+++ resources/views/pages/map/mobileApp/singlePoint/index.blade.php
@@ -1,15 +1,27 @@
 @extends('modules.common.layout.mobileApp.map.index')
 
+@section('layout-styles')
+    <link
+        href="{{ custom_getBuildFilePath('map_mobileApp_singlePoint_index', 'css') }}"
+        rel="stylesheet"
+    >
+    <link
+        href="{{ custom_getBuildFilePath('vendors', 'css') }}"
+        rel="stylesheet"
+    >
+@endsection
+
 @section('layout-content')
     @include('modules.pages.map.mobileApp.singlePoint.index')
 @endsection
 
 @section('layout-scripts')
-    <script src="{{ asset('build/index.bundle.js') }}" defer></script>
-    <script src="{{ asset('build/vendors.bundle.js') }}" defer></script>
-@endsection
-
-@section('layout-styles')
-    <link href="{{ asset('build/index.css') }}" rel="stylesheet">
-    <link href="{{ asset('build/vendors.css') }}" rel="stylesheet">
+    <script
+        defer
+        src="{{ custom_getBuildFilePath('map_mobileApp_singlePoint_index', 'js') }}"
+    ></script>
+    <script
+        defer
+        src="{{ custom_getBuildFilePath('vendors', 'js') }}"
+    ></script>
 @endsection
--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,7 @@ const createConfig = (env, argv) => {
             catalog_secondLevel_index: path.resolve(appDirectoriesData.path_pages, 'catalog', 'secondLevel', 'index'),
             favorites_index: path.resolve(appDirectoriesData.path_pages, 'favorites', 'index'),
             map_web_index: path.resolve(appDirectoriesData.path_pages, 'map', 'web', 'index'),
+            map_mobileApp_singlePoint_index: path.resolve(appDirectoriesData.path_pages, 'map', 'mobileApp', 'singlePoint', 'index'),
             offers_index: path.resolve(appDirectoriesData.path_pages, 'offers', 'index'),
             offers_show: path.resolve(appDirectoriesData.path_pages, 'offers', 'show'),
             profile_organizationInfo_create: path.resolve(appDirectoriesData.path_pages, 'profile', 'organization-info', 'create'),
