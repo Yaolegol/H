300d708e4562dbee6881f9982798e57ed11308ad
Oleg <m160160@yandex.ru>
2023-02-22 21:34:38 +0700

    add list title

--- resources/views/modules/pages/map/web/routes/index/index.blade.php
+++ resources/views/modules/pages/map/web/routes/index/index.blade.php
@@ -13,9 +13,15 @@
             <div class="modules-pages-map-web-routes-index__map-block">
                 @include('modules.common.map.yandex.components.viewAll.index')
             </div>
-            <div class="modules-pages-map-web-routes-index__offers-block">
-                <div class="modules-pages-map-web-routes-index__offers-container">
-                    @include('modules.common.offers.list.index')
+            <div class="modules-pages-map-web-routes-index__offers-area">
+                <div class="modules-pages-map-web-routes-index__offers-block">
+                    <div class="modules-pages-map-web-routes-index__offers-header">
+                        <h4>Товары в видимой области карты*</h4>
+                        <div class="modules-pages-map-web-routes-index__offers-header-description">*чтобы посмотреть другие товары перемещайте или приблизте карту!</div>
+                    </div>
+                    <div class="modules-pages-map-web-routes-index__offers-container">
+                        @include('modules.common.offers.list.index')
+                    </div>
                 </div>
             </div>
         </div>
