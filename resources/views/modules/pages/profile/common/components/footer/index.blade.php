fed99e25ef0cebd66af60ce94c7b125bde592b4d
Oleg <m160160@yandex.ru>
2023-02-25 18:10:24 +0700

    fix submit button message

--- resources/views/modules/pages/profile/common/components/footer/index.blade.php
+++ resources/views/modules/pages/profile/common/components/footer/index.blade.php
@@ -3,7 +3,7 @@
         <button class="button button_form">Сохранить</button>
     </div>
     <div class="modules-pages-profile-common-components-container-footer__info-container-required">
-        <div>* Для сохранения необходимо заполнить все обязательные поля.</div>
+        <div>* Для сохранения необходимо заполнить все обязательные поля</div>
         <div>После заполнения всех обязательных полей данное сообщение будет скрыто</div>
     </div>
     @include('components.form.error.index', [
