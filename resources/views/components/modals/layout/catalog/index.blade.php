b440ca1731877d62da76d34eb724833f206e8251
Oleg <m160160@yandex.ru>
2023-03-10 08:32:34 +0700

    catalog modal - reduce to h4

--- resources/views/components/modals/layout/catalog/index.blade.php
+++ resources/views/components/modals/layout/catalog/index.blade.php
@@ -1,5 +1,5 @@
 <div class="modules-common-catalog j-modules-common-catalog j-components-search-catalog {{$className ?? ''}}">
-    <h2 class="modules-common-catalog__title">{{$title}}</h2>
+    <h4 class="modules-common-catalog__title">{{$title}}</h4>
     <div class="modules-common-catalog__catalog-search-container">
         @include('components.inputs.search.index')
     </div>
