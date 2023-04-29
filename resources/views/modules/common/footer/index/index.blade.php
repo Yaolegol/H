9a3650b9e906fa7b678d8567d843cfb5ef7467ff
Oleg <m160160@yandex.ru>
2023-04-29 17:05:18 +0700

    fix footer

--- resources/views/modules/common/footer/index/index.blade.php
+++ resources/views/modules/common/footer/index/index.blade.php
@@ -1,12 +1,26 @@
 <div class="modules-common-footer-index">
+    <div class="modules-common-footer-index__contacts-area">
+        <div class="modules-common-footer-index__contacts-block-left">
+            <div>Социальные сети</div>
+        </div>
+        <div class="modules-common-footer-index__contacts-block-right">
+            <div>По вопросам сотрудничества, рекламы, пожеланий, предложений и отзывов<br />
+                Вы можете связаться с нами любым удобных способом<br />
+                <a href="tel:89527568210">по телефону: 8 952 756 82 10</a>
+            </div>
+        </div>
+    </div>
     <div class="modules-common-footer-index__info-container">
         <div>
             <a
                 class="modules-common-footer-index__link"
                 href="/legal"
-            >Правила использования сайта и другая правовая информация</a>
+            >Правила пользования сайтом, политики конфиденциальности,
+                <br />хранения и обработки персональных данных, использования файлов cookie
+                <br />и другая правовая информация
+            </a>
         </div>
-        <div class="modules-common-footer-index__copyright-container">© 2023 г. Все права защищены</div>
+        <div class="modules-common-footer-index__copyright-container">© clickferma, 2023. Все права защищены</div>
     </div>
 </div>
 
