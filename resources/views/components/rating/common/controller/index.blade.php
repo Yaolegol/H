f206be254d6aea788c561605c504c038dbe79913
Oleg <m160160@yandex.ru>
2023-05-07 00:05:09 +0700

    add success rating send message

--- resources/views/components/rating/common/controller/index.blade.php
+++ resources/views/components/rating/common/controller/index.blade.php
@@ -8,5 +8,11 @@
     @csrf
 
     <input name="offer_id" type="hidden" value="{{$offerId}}">
-    {{$slot}}
+
+    <div class="j-components-rating-common-controller__content">
+        {{$slot}}
+    </div>
+    <div class="j-components-rating-common-controller__success">
+        Спасибо! Ваш отзыв отправлен!
+    </div>
 </form>
