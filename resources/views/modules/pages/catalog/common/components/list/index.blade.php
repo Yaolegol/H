93d67a69629420040956e85d1647e7e284f9be4a
Oleg <m160160@yandex.ru>
2023-05-14 18:48:35 +0700

    fix catalog items styles

--- resources/views/modules/pages/catalog/common/components/list/index.blade.php
+++ resources/views/modules/pages/catalog/common/components/list/index.blade.php
@@ -2,7 +2,9 @@
     <div class="modules-pages-catalog-common-components-list__items-container">
         @foreach($catalogPage as $catalogItem)
             <div class="modules-pages-catalog-common-components-list__item">
-                @include('modules.pages.catalog.common.components.item.index', [ 'catalogItem' => $catalogItem ])
+                <div class="modules-pages-catalog-common-components-list__item-content">
+                    @include('modules.pages.catalog.common.components.item.index', [ 'catalogItem' => $catalogItem ])
+                </div>
             </div>
         @endforeach
     </div>
