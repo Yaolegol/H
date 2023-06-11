f3173023dac25b416dc2c45131bb3c218d6ed73c
Oleg <m160160@yandex.ru>
2023-06-11 20:03:18 +0700

    fix password validator

--- app/Http/Controllers/helpers/web/authorization/index.php
+++ app/Http/Controllers/helpers/web/authorization/index.php
@@ -268,7 +268,7 @@ function getRegistrationSendSmsValidator($request) {
         $request->all(),
         [
             'agreement' => ['accepted'],
-            'password' => ['required', 'max:25', 'min:6'],
+            'password' => ['required', 'max:50', 'min:6'],
             'password_confirmation' => ['required', 'same:password'],
             'phone' => ['required', 'digits:11', new StartWith('7'), 'unique:users'],
         ],
