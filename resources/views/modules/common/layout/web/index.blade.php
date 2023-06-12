14b1b393f9b83b2a7b6137f46d17f04dd31eb02a
Oleg <m160160@yandex.ru>
2023-06-12 20:40:14 +0700

    add yandex metrika

--- resources/views/modules/common/layout/web/index.blade.php
+++ resources/views/modules/common/layout/web/index.blade.php
@@ -30,12 +30,6 @@
 
         <!-- Styles -->
         @yield('layout-styles')
-
-        <!-- Scripts -->
-        <script
-            src="https://api-maps.yandex.ru/2.1/?apikey=b92366ae-3520-458e-bf9f-17db62817585&lang=ru_RU"
-            type="text/javascript"
-        ></script>
     </head>
     <body class="j-location-controller j-modules-pages-map-web-common-components-filters-product-controller">
         <div class="j-csrf-token" data-value="{{ csrf_token() }}"></div>
@@ -63,5 +57,28 @@
         </div>
         @include('components.modals.base.common.index')
         @yield('layout-scripts')
+
+        <!-- Yandex map -->
+        <script
+            src="https://api-maps.yandex.ru/2.1/?apikey=b92366ae-3520-458e-bf9f-17db62817585&lang=ru_RU"
+            type="text/javascript"
+        ></script>
+
+{{--        <!-- Yandex.Metrika counter -->--}}
+{{--        <script type="text/javascript" >--}}
+{{--            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};--}}
+{{--                m[i].l=1*new Date();--}}
+{{--                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}--}}
+{{--                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})--}}
+{{--            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");--}}
+
+{{--            ym(93934360, "init", {--}}
+{{--                clickmap:true,--}}
+{{--                trackLinks:true,--}}
+{{--                accurateTrackBounce:true--}}
+{{--            });--}}
+{{--        </script>--}}
+{{--        <noscript><div><img src="https://mc.yandex.ru/watch/93934360" style="position:absolute; left:-9999px;" alt="" /></div></noscript>--}}
+{{--        <!-- /Yandex.Metrika counter -->--}}
     </body>
 </html>
