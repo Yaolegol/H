fc0008e6de67b8913f8d9e84db20c2ff51d6a3fd
Oleg <m160160@yandex.ru>
2023-07-30 16:22:35 +0700

    remove useless backup php file

--- database/backup/S3/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once('/home/oleg/PhpstormProjects/Laravel/H/vendor/autoload.php');
-require_once('/home/oleg/PhpstormProjects/Laravel/H/app/Http/Controllers/helpers/common/assets/index.php');
-
-$s3 = S3_STORAGE_getS3Client();
-$s3->upload('clickferma-buckets-mysql', 'backup/common/backup.mysql', file_get_contents(__DIR__ . '/../mysql/backup.mysql'));
