221c708729149c07a4770603e1d5119a2ba95ddd
Oleg <m160160@yandex.ru>
2023-07-30 16:11:46 +0700

    add tabs for backup sh script

--- database/backup/index.sh
+++ database/backup/index.sh
@@ -1,6 +1,11 @@
-sudo mysqldump \
+mysqldump \
     --defaults-file="/home/oleg/Документы/Clickferma/custom.cnf" \
     --all-databases \
     --result-file="/home/oleg/PhpstormProjects/Laravel/H/database/backup/mysql/backup.mysql" \
     && \
-    aws --endpoint-url=https://storage.yandexcloud.net s3 cp /home/oleg/PhpstormProjects/Laravel/H/database/backup/mysql/backup.mysql s3://clickferma-buckets-mysql/backup/common/backup.mysql
+aws \
+    --endpoint-url=https://storage.yandexcloud.net \
+    s3 \
+    cp \
+    /home/oleg/PhpstormProjects/Laravel/H/database/backup/mysql/backup.mysql \
+    s3://clickferma-buckets-mysql/backup/common/backup.mysql
