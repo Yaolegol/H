6ea5e72c02986a4f04ba9e988480f0f2438d498e
Oleg <m160160@yandex.ru>
2023-09-25 14:58:19 +0700

    revert phone inputs for create and edit sale offers

--- resources/views/modules/pages/profile/routes/sale-offers/create/index.blade.php
+++ resources/views/modules/pages/profile/routes/sale-offers/create/index.blade.php
@@ -84,16 +84,16 @@
                     'required' => true,
                     'title' => 'Телефон:'
                     ])
-                    @component('modules.pages.auth.common.components.formItemContainer.index')
-                        @include('components.inputs.phone.index', [
-                                        'name' => 'phone',
-                                        'required' => true,
-                                        'value' => old('phone'),
-                        ])
-                        @include('components.form.error.index', [
-                            'message' => $errors->first('phone'),
-                        ])
-                    @endcomponent
+                    @include('components.inputs.form.index', [
+                                    'name' => 'phone',
+                                    'placeholder' => 'Телефон',
+                                    'required' => true,
+                                    'type' => 'tel',
+                                    'value' => old('phone'),
+                                ])
+                    @include('components.form.error.index', [
+                        'message' => $errors->first('phone'),
+                    ])
                 @endcomponent
 
                 @component('modules.pages.profile.common.components.container.form-field.index', ['title' => 'Режим работы:'])
