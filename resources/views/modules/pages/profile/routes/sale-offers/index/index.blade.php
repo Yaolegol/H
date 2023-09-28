7bbd5fd5aae0e5fe7e9789c62fff2ffa1906f1f6
Oleg <m160160@yandex.ru>
2023-09-28 15:48:15 +0700

    fix empty message for profile create sale offer

--- resources/views/modules/pages/profile/routes/sale-offers/index/index.blade.php
+++ resources/views/modules/pages/profile/routes/sale-offers/index/index.blade.php
@@ -36,18 +36,7 @@
                         @endforeach
                     @else
                         <div class="modules-pages-profile-routes-sale-offers-info-index__empty-block">
-                            <div>У Вас пока не создано торговых предложений</div>
-                            <div class="modules-pages-profile-routes-sale-offers-info-index__empty-container">Чтобы добавить товары, нажмите
-                                <a
-                                    class="modules-pages-profile-routes-sale-offers-info-index__empty-link"
-                                    href="/profile/sale-offers/create"
-                                >
-                                    "Добавить товары"
-                                </a>
-                            </div>
-                            <div class="modules-pages-profile-routes-sale-offers-info-index__empty-container modules-pages-profile-routes-sale-offers-info-index__empty-container_bold">
-                                Это просто и бесплатно!
-                            </div>
+                            <div>Добавьте товары - <span style="font-weight: bold">это просто и бесплатно!</span></div>
                         </div>
                     @endif
                 @endcomponent
