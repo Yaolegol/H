ced6eff31a948aa63eaa08869b31e4c04a3fe3bc
Oleg <m160160@yandex.ru>
2023-09-28 18:53:18 +0700

    add open balloon after click map page card show on map

--- resources/views/modules/common/map/yandex/components/viewAll/index.js
+++ resources/views/modules/common/map/yandex/components/viewAll/index.js
@@ -101,13 +101,22 @@ class MapYandexComponentsViewAll {
         });
     }
 
-    handleShowPlacemark = (e) => {
+    handleShowPlacemark = async (e) => {
         const geoQueryResult = ymaps.geoQuery(this.mapCluster.getGeoObjects());
         const geoQueryResultPlacemarks = geoQueryResult.search(`properties.id = "${e.detail.placemarkId}"`);
+        const placemark = geoQueryResultPlacemarks.get(0);
 
-        this.mapInstance.setCenter(geoQueryResultPlacemarks.get(0).geometry.getCoordinates(), 17, {
+        if(!placemark) {
+            return;
+        }
+
+        const {balloon, geometry} = placemark;
+
+        await this.mapInstance.setCenter(geometry.getCoordinates(), 17, {
             duration: 1000,
         });
+
+        balloon.open();
     }
 
     handleUpdateGeo = (e) => {
