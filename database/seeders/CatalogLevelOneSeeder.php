9b141be21c4a2955c6c2f8c824c97531dfd1e047
Oleg <m160160@yandex.ru>
2023-09-29 12:30:46 +0700

    add fur for catalog

--- database/seeders/CatalogLevelOneSeeder.php
+++ database/seeders/CatalogLevelOneSeeder.php
@@ -198,6 +198,13 @@ class CatalogLevelOneSeeder extends Seeder
             'title' => 'Удобрения',
         ],
         [
+            'image' => 'public/catalog/levelOne/items/furs/images/main/1.jpg',
+            'image_licence_link' => 'Photo by <a href="https://unsplash.com/@myfunkypixel?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Vita Leonis</a> on <a href="https://unsplash.com/photos/jvn8gPiGEac?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>',
+            'link' => 'furs',
+            'order' => 28,
+            'title' => 'Пушнина',
+        ],
+        [
             'id' => 999,
             'image' => 'public/catalog/levelOne/items/other/images/main/1.jpg',
             'image_licence_link' => 'Изображение от <a style="text-decoration: underline" target="_blank" rel="noopener noreferrer" href="https://ru.freepik.com/free-photo/glasses-on-market-in-morocco_4246624.htm#page=2&query=%D0%9D%D0%B0%D1%86%D0%B8%D0%BE%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B5%20%D1%81%D1%83%D0%B2%D0%B5%D0%BD%D0%B8%D1%80%D1%8B&position=6&from_view=search&track=ais">Freepik</a>',
