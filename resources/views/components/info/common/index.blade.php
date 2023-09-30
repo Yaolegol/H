220b58d978c3d06878731131b79d031ca3c48ec0
Oleg <m160160@yandex.ru>
2023-09-30 14:45:45 +0700

    add share component

--- resources/views/components/info/common/index.blade.php
+++ resources/views/components/info/common/index.blade.php
@@ -4,43 +4,7 @@
 >
     <div class="components-info-common__text">{!! ($text ?? '') !!}</div>
     <div class="components-info-common__message-area">
-        <div class="components-info-common__contacts-messenger-container">
-            <a
-                href="https://vk.com/share.php?url=https://clickferma.ru"
-                rel="noopener noreferrer"
-                target="_blank"
-            >
-                @include('icons.social.vk')
-            </a>
-        </div>
-        <div class="components-info-common__contacts-messenger-container">
-            <a
-                href="https://connect.ok.ru/offer?url=https://clickferma.ru"
-                rel="noopener noreferrer"
-                target="_blank"
-            >
-                @include('icons.social.ok')
-            </a>
-        </div>
-        <div class="components-info-common__contacts-messenger-container">
-            <a
-                href="https://t.me/share/url?url=https://clickferma.ru"
-                rel="noopener noreferrer"
-                target="_blank"
-            >
-                @include('icons.messengers.telegram')
-            </a>
-        </div>
-        <div class="components-info-common__contacts-messenger-container">
-            <a href="https://wa.me/?text={{urlencode('https://clickferma.ru - натуральные фермерские продукты')}}" rel="noopener noreferrer" target="_blank">
-                @include('icons.messengers.whatsapp')
-            </a>
-        </div>
-        <div class="components-info-common__contacts-messenger-container">
-            <a href="viber://forward?text={{urlencode('https://clickferma.ru - натуральные фермерские продукты')}}" rel="noopener noreferrer" target="_blank">
-                @include('icons.messengers.viber')
-            </a>
-        </div>
+        @include('components.share.common.index')
     </div>
     <button
         class="components-info-common__button j-components-info-common__close-button"
