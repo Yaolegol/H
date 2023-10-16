56c691686ec633a03c9c1fcc4ef6975135e78765
Oleg <m160160@yandex.ru>
2023-10-16 19:58:10 +0700

    add create sale offer checkmark for categories

--- resources/views/components/buttons/category/common/group/index.blade.php
+++ resources/views/components/buttons/category/common/group/index.blade.php
@@ -1,6 +1,7 @@
 <div class="components-buttons-category-common-group">
     @foreach($list as $item)
-        <div class="components-buttons-category-common-group__item">
+        <div class="components-buttons-category-common-group__item j-modules-pages-profile-routes-sale-offers-common-categories-controller__item">
+            <span class="components-buttons-category-common-group__item-arrow"></span>
             @include('components.buttons.category.common.single.index', [
                 'className' => $classNameButton,
                 'id' => $item['value'],
