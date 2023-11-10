a03d93955dca936dba1d35e866ffe12af2c5dfb6
Oleg <m160160@yandex.ru>
2023-11-11 04:56:17 +0700

    fix socials

--- resources/views/components/contacts/links/channels/index.blade.php
+++ resources/views/components/contacts/links/channels/index.blade.php
@@ -1,11 +1,11 @@
 <div class="components-contacts-links-channels">
     <div class="components-contacts-links-channels__container">
-        <a href="https://t.me/101ferma" rel="noopener noreferrer" target="_blank">
+        <a href="https://t.me/ferma101" rel="noopener noreferrer" target="_blank">
             @include('icons.messengers.telegram')
         </a>
     </div>
     <div class="components-contacts-links-channels__container">
-        <a href="https://vk.com/vk101ferma" rel="noopener noreferrer" target="_blank">
+        <a href="https://vk.com/ferma101" rel="noopener noreferrer" target="_blank">
             @include('icons.social.vk')
         </a>
     </div>
